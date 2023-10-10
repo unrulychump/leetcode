08ee23a9b2b4398783956441ae0d72f7c6d2d0e1
w18767696951 <593861484@qq.com>
2023-10-10 13:22:08 +0800

    意外

---
+++ src/day4/class1.java
@@ -0,0 +1,16 @@
+package day4;
+
+/**
+ * 字串问题合集
+ * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+ *
+ * 子数组是数组中元素的连续非空序列。
+ */
+//前缀和hash结合的问题
+public class class1 {
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+
+        }
+    }
+}
